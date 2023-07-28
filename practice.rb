09e0c3d4cf41c558eb5f309d41744222f7b0c7c2
Eric Belongea <eric.belongea@gmail.com>
2023-07-28 12:35:24 -0600

    Another file

---
+++ practice.rb
@@ -0,0 +1,10 @@
+# Ruby
+name = "Eric"
+age = 26
+money = 1.20
+
+puts name
+puts age
+puts money
+
+puts "Hello #{name} you have #{money} in your bank account at #{age} years old. You need to get your funds up."
